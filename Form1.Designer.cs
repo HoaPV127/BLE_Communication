dcb55c5cd63758c85bdbd35294ef9d06cdceb7e9
HoaPV127 <hoa.pv127@gmail.com>
2022-05-12 23:33:40 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace BLE_Communication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.msgRTbx = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sendTbx = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // msgRTbx
+            // 
+            this.msgRTbx.Location = new System.Drawing.Point(10, 10);
+            this.msgRTbx.Name = "msgRTbx";
+            this.msgRTbx.Size = new System.Drawing.Size(300, 300);
+            this.msgRTbx.TabIndex = 0;
+            this.msgRTbx.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // sendTbx
+            // 
+            this.sendTbx.Location = new System.Drawing.Point(10, 316);
+            this.sendTbx.Name = "sendTbx";
+            this.sendTbx.Size = new System.Drawing.Size(218, 20);
+            this.sendTbx.TabIndex = 2;
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(234, 316);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 3;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(332, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Available devices";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 346);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.sendTbx);
+            this.Controls.Add(this.msgRTbx);
+            this.Name = "Form1";
+            this.Text = "BLE Devices";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox msgRTbx;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox sendTbx;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth;
+using Windows.Devices;
+using Windows.Foundation;
+using Windows;
+using Windows.Storage.Streams;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using System.Threading;
+using Windows.UI.Core;
+using Windows.ApplicationModel.Core;
+
+namespace BLE_Communication
+{
+    public partial class Form1 : Form
+    {
+
+        //private BluetoothLEAdvertisementPublisher m_publisher;
+        private BluetoothLEAdvertisementWatcher m_watcher;
+        //private Dictionary<ulong, BluetoothLEDevice> m_devices;
+        private List<ulong> m_devices;
+        private BluetoothLEDevice currDevice;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+            //m_devices = new Dictionary<ulong, BluetoothLEDevice>();    
+            m_devices = new List<ulong>();
+            m_watcher = new BluetoothLEAdvertisementWatcher();
+
+            Console.WriteLine("Start Scan");
+            //m_watcher.AdvertisementFilter = 
+            m_watcher.ScanningMode = BluetoothLEScanningMode.Active;
+            m_watcher.Received += OnAdvertisementReceived;
+            m_watcher.Start();
+
+        }
+        private async void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs)
+        {
+            string devName = null;
+            //AddScanedDevice(eventArgs.BluetoothAddress);
+            var manu = eventArgs.Advertisement.ManufacturerData[0].CompanyId;
+            ulong devAddr = eventArgs.BluetoothAddress;
+            
+            //BluetoothLEDevice scanDevice = await BluetoothLEDevice.FromBluetoothAddressAsync(devAddr);
+            //devName = scanDevice.Name;
+
+            //if (m_devices.ContainsKey(eventArgs.BluetoothAddress) == false)
+            if (m_devices.Contains(devAddr) == false)
+            {
+                //m_devices.Add(eventArgs.BluetoothAddress, device);
+                m_devices.Add(devAddr);
+
+                Button dev = new Button();
+
+                if (string.IsNullOrEmpty(devName))
+                    dev.Text = "Unknown";
+                else
+                    dev.Text = devName;
+                dev.Width = 150;
+                dev.Height = 30;
+                dev.Location = new System.Drawing.Point(this.Width - 190, 30 * m_devices.Count);
+                dev.Click += (sender, EventArgs) => { SelectAndConnect(sender, EventArgs, devAddr); };
+
+                if (this.InvokeRequired)
+                {
+                    Invoke(new Action(() =>
+                    {
+                        this.Controls.Add(dev);
+                    }));
+                }
+                else
+                {
+                    this.Controls.Add(dev);
+                }
+
+            }
+        }
+
+        private async void SelectAndConnect(object sender, EventArgs e, ulong addr)
+        {
+            //BluetoothLEDevice device = m_devices[addr];
+            //currDevice = device;
+            currDevice = await BluetoothLEDevice.FromBluetoothAddressAsync(addr);
+            BluetoothLEDevice device = currDevice;
+
+            if (device.ConnectionStatus == BluetoothConnectionStatus.Connected)
+            {
+                return;
+            }
+
+            DeviceAccessStatus status = await device.RequestAccessAsync();
+
+            if (status == DeviceAccessStatus.Allowed)
+            {
+                if (this.msgRTbx.InvokeRequired)
+                {
+                    Invoke(new Action(() =>
+                    {
+                        this.msgRTbx.AppendText($"Connected to {device.Name}\n");
+                    }));
+                }
+                else
+                {
+                    this.msgRTbx.AppendText($"Connected to {device.Name}\n");
+                }
+            }
+
+            GattDeviceServicesResult devService = await device.GetGattServicesAsync();
+
+            if (devService.Status == GattCommunicationStatus.Success)
+            {
+                var services = devService.Services;
+
+                foreach (var service in services)
+                {
+                    if (!service.Uuid.ToString().Contains("0000ffe0-"))
+                    {
+                        continue;
+                    }
+                    GattCharacteristicsResult devCharacterist = await service.GetCharacteristicsAsync();
+
+                    if (devCharacterist.Status == GattCommunicationStatus.Success)
+                    {
+                        foreach (var characteristic in devCharacterist.Characteristics)
+                        {
+                            GattCharacteristicProperties devProperties = characteristic.CharacteristicProperties;
+
+                            if (devProperties.HasFlag(GattCharacteristicProperties.Notify))
+                            {
+                                GattCommunicationStatus commStatus = GattCommunicationStatus.AccessDenied;
+
+                                try
+                                {
+                                    commStatus = await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
+                                                GattClientCharacteristicConfigurationDescriptorValue.Notify);
+                                }
+                                catch (Exception ex)
+                                {
+                                    if (this.msgRTbx.InvokeRequired)
+                                    {
+                                        Invoke(new Action(() =>
+                                        {
+                                            this.msgRTbx.AppendText(ex.Message);
+                                        }));
+                                    }
+                                    else
+                                    {
+                                        this.msgRTbx.AppendText(ex.Message);
+                                    }
+                                }
+                                if (commStatus == GattCommunicationStatus.Success)
+                                {
+                                    characteristic.ValueChanged += Characteristic_ValueChanged;
+                                    // Server has been informed of clients interest.
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        void Characteristic_ValueChanged(GattCharacteristic eventSender, GattValueChangedEventArgs args)
+        {
+            var reader = DataReader.FromBuffer(args.CharacteristicValue);
+
+            var value = reader.ReadString(args.CharacteristicValue.Length);
+
+            if (this.msgRTbx.InvokeRequired)
+            {
+                Invoke(new Action(() =>
+                {
+                    this.msgRTbx.AppendText($"{currDevice.Name}: {value}\n");
+                }));
+            }
+            else
+            {
+                this.msgRTbx.AppendText($"{currDevice.Name}: {value}\n");
+            }
+        }
+
+        private async void sendBtn_Click(object sender, EventArgs e)
+        {
+            string sendText = this.sendTbx.Text.ToString();
+
+            var writer = new DataWriter();
+            // WriteByte used for simplicity. Other common functions - WriteInt16 and WriteSingle
+            writer.WriteString(sendText);
+            GattCommunicationStatus result = GattCommunicationStatus.Unreachable;
+
+            GattDeviceServicesResult devService = await currDevice.GetGattServicesAsync();
+
+            if (devService.Status == GattCommunicationStatus.Success)
+            {
+
+
+                var services = devService.Services;
+
+                foreach (var service in services)
+                {
+                    if (!service.Uuid.ToString().Contains("0000ffe0-"))
+                    {
+                        continue;
+                    }
+                    GattCharacteristicsResult devCharacterist = await service.GetCharacteristicsAsync();
+
+                    if (devCharacterist.Status == GattCommunicationStatus.Success)
+                    {
+                        foreach (var characteristic in devCharacterist.Characteristics)
+                        {
+                            GattCharacteristicProperties devProperties = characteristic.CharacteristicProperties;
+
+                            if (devProperties.HasFlag(GattCharacteristicProperties.WriteWithoutResponse))
+                            {
+                                try
+                                {
+                                    result = await characteristic.WriteValueAsync(writer.DetachBuffer());
+                                }
+                                catch (Exception ex)
+                                {
+                                    if (this.msgRTbx.InvokeRequired)
+                                    {
+                                        Invoke(new Action(() =>
+                                        {
+                                            this.msgRTbx.AppendText($"{ex.Message}\n");
+                                        }));
+                                    }
+                                    else
+                                    {
+                                        this.msgRTbx.AppendText($"{ex.Message}\n");
+                                    }
+                                }
+
+                                if (result == GattCommunicationStatus.Success)
+                                {
+                                    if (this.msgRTbx.InvokeRequired)
+                                    {
+                                        Invoke(new Action(() =>
+                                        {
+                                            this.msgRTbx.AppendText($"Send to {currDevice.Name}: {sendText}\n");
+                                        }));
+                                    }
+                                    else
+                                    {
+                                        this.msgRTbx.AppendText($"Send to {currDevice.Name}: {sendText}\n");
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
